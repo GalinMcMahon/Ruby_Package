b31eb7e01946f43f11bbeb3b2f473137dba2515f
Galin McMahon and Susha Dore <galin@>
2017-04-24 11:33:44 -0700

    calculate cost to ship package based on volume and weight, passes second spec

--- lib/package.rb
+++ lib/package.rb
@@ -8,4 +8,14 @@ class Package
   define_method(:volume) do
     @height * @width * @length
   end
+
+  define_method(:cost_to_ship) do |weight|
+    if weight <= 5
+      @height * @width * @length * 1/10
+    elsif
+      weight > 5
+      weight_cost = (weight-5.0) * 1/2
+      weight_cost+(@height * @width * @length * 1/10)
+    end
+  end
 end
--- spec/package_spec.rb
+++ spec/package_spec.rb
@@ -8,4 +8,10 @@ describe('Package') do
       expect(new_package.volume()).to(eq(120))
     end
   end
+  describe('#cost_to_ship') do
+    it('calculates the cost of a package based on volume') do
+      new_package = Package.new(4,5,6)
+      expect(new_package.cost_to_ship(6)).to(eq(12.50))
+    end
+  end
 end
